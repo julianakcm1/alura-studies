3edb6a381fc46ba92918efc0ff85c77619af34d7
Juliana <jkcm@ic.ufal.br>
2023-11-03 00:21:52 -0300

    using CSS Modules

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Button from '../components/Button';
 import Form from '../components/Form';
 import List from '../components/List';
-import './style.scss';
+import style from './App.module.scss';
 
 function App() {
   return (
-    <div className="AppStyle">
+    <div className={style.AppStyle}>
       <Form />
       <List />
     </div>
