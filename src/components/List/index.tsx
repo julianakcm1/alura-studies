157bb68079ef46e98adc4da4852cb7a8d11f902f
Juliana <jkcm@ic.ufal.br>
2023-11-13 20:06:24 -0300

    refactoring and adding the Item component

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import style from './List.module.scss';
+import Item from "./Item";
 
 function List() {
   const tarefas = [{
@@ -18,10 +19,14 @@ function List() {
       <h2> Estudos do dia </h2>
       <ul>
         {tarefas.map((item, index) => (
-          <li key={index} className={style.item}>
-            <h3> {item.tarefa} </h3>
-            <span> {item.tempo} </span>
-          </li>
+          <Item 
+            key={index}
+            {...item}
+          />
+          // <li key={index} className={style.item}>
+          //   <h3> {item.tarefa} </h3>
+          //   <span> {item.tempo} </span>
+          // </li>
         ))}
       </ul>
     </aside>
