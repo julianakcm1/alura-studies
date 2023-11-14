30431d87d2f4b609ecd5376e276ce0f9f1d39d2c
Juliana <jkcm@ic.ufal.br>
2023-11-14 00:17:14 -0300

    introducing states

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,32 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import style from './List.module.scss';
 import Item from "./Item";
 
 function List() {
-  const tarefas = [{
+  const [tarefas, setTarefas] = useState([{
     tarefa: 'React',
     tempo: '02:00:00'
-  }, {
-    tarefa: 'Javascript',
+}, {
+    tarefa: 'JavaScript',
     tempo: '01:00:00'
-  }, {
+}, { 
     tarefa: 'Typescript',
     tempo: '03:00:00'
-  }]
+}]);
 
   return (
     <aside className={style.listaTarefas}>
-      <h2> Estudos do dia </h2>
+      <h2 onClick={() => {
+        setTarefas([...tarefas, {tarefa: "Estudar estado", tempo: "05:00:00"}])
+      }}> Estudos do dia </h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item 
             key={index}
             {...item}
           />
-          // <li key={index} className={style.item}>
-          //   <h3> {item.tarefa} </h3>
-          //   <span> {item.tempo} </span>
-          // </li>
         ))}
       </ul>
     </aside>
--- src/components/Stopwatch/index.tsx
+++ src/components/Stopwatch/index.tsx
@@ -7,9 +7,9 @@ export default function Stopwatch() {
   return(
     <div className={style.stopwatch}>
       <p className={style.title}> Escolha um card e inicie o cronômetro </p>
-    <div className={style.clockWrapper}>
-      <Clock />
-    </div>
+      <div className={style.clockWrapper}>
+        <Clock />
+      </div>
       <Button>
         Começar!
       </Button>
